820ee9601809ba94babf8c679be17132d7aa6ae0
Ali Binabaji <binabaji@amwajco.net>
2023-07-07 14:01:52 +0430

    add slider

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TopBar from './components/topBar/TopBar.vue'
+import Slider from './components/slider/Slider.vue'
 </script>
 
 <template>
   <header>
     <TopBar />
+    <Slider />
   </header>
 </template>
---
+++ src/components/slider/Slider.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { register } from 'swiper/element/bundle';
+  register();
+  const spaceBetween = 10;
+</script>
+
+<template>
+  <swiper-container
+    :slides-per-view="1"
+    :space-between="spaceBetween"
+    :centered-slides="true"
+    :loop="true"
+    :autoplay="true"
+    >
+    <swiper-slide>
+      <img class="m-auto" alt="binasaffron" src="@/assets/slider/binasaffron-slider-1.jpg" />
+    </swiper-slide>
+    <swiper-slide>
+      <img class="m-auto"  alt="binasaffron" src="@/assets/slider/binasaffron-slider-2.jpg" />
+    </swiper-slide>
+  </swiper-container>
+</template>
