b8760d6eb7d7ab334cc18f80899caaee3d0a67e4
Ali Binabaji <binabaji@amwajco.net>
2023-07-27 17:33:22 +0430

    add contact us page

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,6 +1,19 @@
-<script setup lang="ts">
-</script>
-
 <template>
-    <div>test</div>
+    <div class="contacUs grid m-4">
+        <div class="mb-3 grid">
+            <span class="text-xl mb-2">آدرس:</span>
+            <span>مشهد ، خیابان شیرازی ، بازار بزرگ غدیر ، طبقه +1 ، پلاک 17</span>
+        </div>
+        <div class="mb-3 grid">
+            <span class="text-xl mb-2">تلفن تماس:</span>
+            <a href="tel:09152163991">09152163991</a>
+        </div>
+        <div class="mb-3 grid">
+            <span class="text-xl mb-2">ایمیل:</span>
+            <a href = "mailto: info@binasaffron.ir">info@binasaffron.ir</a>
+        </div>
+    </div>
+    <div class="m-4 h-80">
+        <iframe class="w-full h-full" src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d25727.67500124207!2d59.611916!3d36.2890356!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x3f6c915e173385f5%3A0x724530cc2acd05db!2z2LLYudmB2LHYp9mGINio24zZhtinIFJhemF2aSBLaG9yYXNhbiBQcm92aW5jZSwgTWFzaGhhZCBEaXN0cmljdCBTYW1lbiBVbm5hbWVkIFJvYWQsIDdKUTYrSlE34oCt!3m2!1d36.2890356!2d59.611916!5e0!3m2!1sen!2s!4v1690462253833!5m2!1sen!2s" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
+    </div>
 </template>
